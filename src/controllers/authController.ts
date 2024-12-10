5b3738f3d5df6f0014ff7ecb2e4c21b6290b4ca3
bewe <93899302+bernarduswillson@users.noreply.github.com>
2024-12-10 20:05:08 +0700

    feat: add get and update user details

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -95,3 +95,81 @@ export const login = async (req: Request, res: Response): Promise<void> => {
     return;
   }
 };
+
+// Get user details
+export const getUser = async (req: Request, res: Response): Promise<void> => {
+  const { user_id } = req.body;
+
+  try {
+    // Users document
+    const userDoc = firestore.collection("users").doc(user_id);
+    const userSnapshot = await userDoc.get();
+    if (!userSnapshot.exists) {
+      res.status(404).json({
+        status: 'error',
+        message: `User ${user_id} not found`,
+      });
+      return;
+    }
+
+    // Retrieve user details
+    const user = userSnapshot.data() as User;
+
+    res.status(200).json({
+      status: 'success',
+      message: 'User details retrieved successfully',
+      user,
+    });
+    return;
+  } catch (error) {
+    console.error("Error retrieving user:", error);
+    res.status(500).json({
+      status: 'error',
+      message: 'Failed to retrieve user details',
+      error: (error instanceof Error) ? error.message : 'Unknown error',
+    });
+    return;
+  }
+}
+
+// Update user details
+export const updateUser = async (req: Request, res: Response): Promise<void> => {
+  const { user_id } = req.body;
+  const { name, email, address } = req.body;
+
+  try {
+    // Users document
+    const userDoc = firestore.collection("users").doc(user_id);
+    const userSnapshot = await userDoc.get();
+    if (!userSnapshot.exists) {
+      res.status(404).json({
+        status: 'error',
+        message: `User ${user_id} not found`,
+      });
+      return;
+    }
+
+    // Update user details
+    const userUpdate = {
+      name,
+      email,
+      address,
+    };
+    await userDoc.update(userUpdate);
+
+    res.status(200).json({
+      status: 'success',
+      message: 'User details updated successfully',
+      user: userUpdate,
+    });
+    return;
+  } catch (error) {
+    console.error("Error updating user:", error);
+    res.status(500).json({
+      status: 'error',
+      message: 'Failed to update user details',
+      error: (error instanceof Error) ? error.message : 'Unknown error',
+    });
+    return;
+  }
+};
--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,9 +1,12 @@
 import express from 'express';
+import { authMiddleware } from '../middleware/authMiddleware';
 
 // Controllers
 import {
   register,
-  login
+  login,
+  getUser,
+  updateUser,
 } from '../controllers/authController';
 
 const router = express.Router();
@@ -11,5 +14,7 @@ const router = express.Router();
 // Routes
 router.post('/register', register);
 router.post('/login', login);
+router.get('/user', authMiddleware, getUser);
+router.put('/user', authMiddleware, updateUser);
 
 export default router;
